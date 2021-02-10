edb4bd2f075a6d4ea5afd1b484256c26a3f6dae1
ORI <olivier.rigah54@gmail.com>
2021-02-10 17:03:44 +0100

    Modif et ajouts des pages

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -11,7 +11,23 @@ class FirstController extends AbstractController
     #[Route('/first', name: 'first')]
     public function index(): Response
     {
+        return $this->render('first/first.html.twig', [
+            'controller_name' => 'FirstController',
+        ]);
+    }
+
+    #[Route('/', name: 'accueil')]
+    public function accueil(): Response
+    {
         return $this->render('first/index.html.twig', [
+            'controller_name' => 'FirstController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('first/contact.html.twig', [
             'controller_name' => 'FirstController',
         ]);
     }
