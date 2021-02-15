844f4dfea8bb4d91e8c718a1d65297f6a350b9f6
ORI <olivier.rigah54@gmail.com>
2021-02-15 14:07:41 +0100

    Modif CSS JS 1502 midi

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,56 @@
 console.log('CODE JS CHARGE...');
+
+// $(document).ready(function(){
+
+
+//     // NAV SLIDE TOOGLE
+//     $('i.icon').click(function(){
+//         $('.nav-list').slideToggle()
+
+//     });
+
+//     // Sticky NAV BAR
+//     $(window).scroll(function(){
+//         var sc = $(this).scrollTop();
+//         if(sc > 50){
+//             $('header').addClass('sticky');
+            
+//         }else{
+//             $('header').removeClass('sticky');
+//         }
+//     });
+
+// });
+
+
+// $(function() {
+
+    
+//     $("img.lightbox").on("click", lightbox);
+// });
+
+
+// function lightbox()
+// {
+   
+//     let src = $(this).attr("src");
+
+   
+//     let overlay = `<div class="overlay"><img src="${src}" alt="Aperçu de mon image"></div>`;
+
+   
+//     $("body").append(overlay);
+
+   
+//     $(".overlay").fadeIn().css("display", "flex").on("click", closeLightbox);
+// }
+
+
+// function closeLightbox()
+// {
+    
+//     $(this).fadeOut("slow", function() {
+        
+//         $(this).remove();
+//     });
+// }
