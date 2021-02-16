b17b177465b06dce78803c40578e83722396132c
ORI <olivier.rigah54@gmail.com>
2021-02-16 16:06:10 +0100

    ajout formulaire de creation annonce dans epsace membre

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -5,14 +5,42 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+// ne pas oublier le use
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Annonce;
+use App\Form\AnnonceType;
 
 class MembreController extends AbstractController
 {
-    #[Route('/membre', name: 'membre')]
-    public function index(): Response
+    #[Route('/membre', name: 'membre', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
     {
+        $annonce = new Annonce();
+        $form = $this->createForm(AnnonceType::class, $annonce);
+        $form->handleRequest($request);
+
+        $messageConfirmation = "";
+        if ($form->isSubmitted() && $form->isValid()
+        ) {
+            // compléter les infos manquantes
+            $annonce->setDatePublication(new \DateTime());
+            // ajouter l'auteur de l'annonce avec l'utilisateur connecté
+            $userConnecte = $this->getUser();
+            $annonce->setUser($userConnecte);
+
+            // code qui insère la nouvelle ligne dans SQL
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($annonce);
+            $entityManager->flush();
+
+            $messageConfirmation = "votre annonce est publiée";
+        }
+
+
         return $this->render('membre/index.html.twig', [
-            'controller_name' => 'MembreController',
+            'annonce' => $annonce,
+            'form' => $form->createView(),
+            'messageConfirmation' => $messageConfirmation,
         ]);
     }
 }
